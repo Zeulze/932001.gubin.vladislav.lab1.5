35d6d8c8fdc5e8373b3db51b3247af0564398d3a
Vladislav Gubin <49865020+Zeulze@users.noreply.github.com>
2022-10-21 23:58:19 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 
 const box = document.querySelectorAll('.box'),
       btn = document.querySelectorAll('button'),
+      main = document.querySelector('main'),
       poppedBox = document.querySelector('.popped-box'),
       newsText = poppedBox.querySelector('.popped-box-text'),
       newsHeader = poppedBox.querySelector('.popped-box-header');
@@ -13,7 +14,7 @@ const specialText = [
 ]
       
 const mainText = `<br>
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.`;
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam hic, ipsa, ullam, cupiditate eveniet at voluptate corrupti commodi nobis ratione voluptatem! Vel animi totam cupiditate doloribus ad ab exercitationem officia eveniet impedit? Deleniti quasi nisi consectetur perspiciatis quibusdam nostrum, enim perferendis nam, magni molestias recusandae id libero vitae, repudiandae praesentium.`;
 
 function toHover(event) {
     event.currentTarget.style.backgroundColor = 'red';
@@ -23,35 +24,85 @@ function toUnHover(event) {
     event.currentTarget.style.backgroundColor = 'aqua';
 }
 
-box.forEach(item => {
-    item.addEventListener('mouseover', toHover);
-    item.addEventListener('mouseout', toUnHover);
+let helper = false;
+
+main.addEventListener('mouseover', (e) => {
+    box.forEach(item => {
+        if (e.target == item && !helper) {
+            item.addEventListener('mouseover', toHover);
+            // item.style.backgroundColor = 'red';
+        }
+        else {
+            item.addEventListener('mouseout', toUnHover);
+        }
+    });
 });
 
-document.addEventListener('mousemove', () => {
-    if (poppedBox.style.display != 'block') {
-        box.forEach(item => {
-            item.style.opacity = "100%"
-        });
-    }
-});
+main.addEventListener('click', (e) => {
+    btn.forEach((item, index) => {
+        if (e.target == item) {
+            box.forEach(item => {
+                item.style.opacity = '25%'
+                item.style.backgroundColor ='aqua';
+                item.removeEventListener('mouseover', toHover);
+            });
+
+            poppedBox.style.display = 'block';
+            
+            helper = true;
+            
+            newsHeader.innerHTML = `Новость ${index + 1}`;
+            newsText.innerHTML = `Новость ${index + 1}: ${specialText[index]}${mainText}`;
+        }
+    });
+    poppedBox.addEventListener('click', e => {
+        poppedBox.style.display = 'none';
+        box.forEach(item => item.style.opacity = '100%');
+        helper = false;
+    })
+})
 
 
-btn.forEach((item, index) => {
-    item.addEventListener('click', () => {
-        box.forEach(item => {
-            item.style.opacity = '25%';
-            item.style.backgroundColor = 'aqua';
-        });
 
-        poppedBox.style.display = 'block';
 
-        newsText.innerHTML = `Новость ${index + 1}: ${specialText[index]}${mainText}`;
-        newsHeader.innerHTML = `Новость ${index + 1}`;
-    }); 
-});
 
-document.addEventListener('mousemove', () => poppedBox.style.display = "none");
+
+
+
+
+
+
+
+
+// box.forEach(item => {
+//     item.addEventListener('mouseover', toHover);
+//     item.addEventListener('mouseout', toUnHover);
+// });
+
+// document.addEventListener('mousemove', () => {
+//     if (poppedBox.style.display != 'block') {
+//         box.forEach(item => {
+//             item.style.opacity = "100%"
+//         });
+//     }
+// });
+
+
+// btn.forEach((item, index) => {
+//     item.addEventListener('click', () => {
+//         box.forEach(item => {
+//             item.style.opacity = '25%';
+//             item.style.backgroundColor = 'aqua';
+//         });
+
+//         poppedBox.style.display = 'block';
+
+//         newsText.innerHTML = `Новость ${index + 1}: ${specialText[index]}${mainText}`;
+//         newsHeader.innerHTML = `Новость ${index + 1}`;
+//     }); 
+// });
+
+// document.addEventListener('mousemove', () => poppedBox.style.display = "none");
 
 
 
